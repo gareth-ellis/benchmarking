f320a40eaaff3720b063d904bea7690111df2312
Gareth Ellis <gareth@gsellis.com>
2018-07-01 08:38:21 +0200

    typos and getting build to fail if node fails
    Also removes the deletion of node while testing

--- experimental/benchmarks/community-benchmark/summarise.sh
+++ experimental/benchmarks/community-benchmark/summarise.sh
@@ -27,7 +27,7 @@ echo "RUNS = defaults to 1"
 echo "FILTER = defaults to empty"
 
 }
-start=`date +%s`
+startTime=`date +%s`
 getMACHINE_THREADS=`cat /proc/cpuinfo |grep processor|tail -n1|awk {'print $3'}`
 let getMACHINE_THREADS=getMACHINE_THREADS+1 #getting threads this way is 0 based. Add one
 optional MACHINE_THREADS $getMACHINE_THREADS
@@ -35,29 +35,35 @@ optional CATEGORY
 optional RUNS  1
 optional FILTER
 optional BRANCH master
-rm -rf node
-git clone http://github.com/nodejs/node.git
+pushd $startDir
+#rm -rf node
+if [ ! -d node ]; then
+	git clone http://github.com/nodejs/node.git
+fi
 cd node
 git checkout $BRANCH
 lastCommit=`git log --format="%H" -n 1`
 ./configure  > ../node-master-build.log
 make -j${MACHINE_THREADS}  >> ../node-master-build.log 2>&1
-mv out/Release/node ./node
 if [ $? -eq 0 ]; then
 	echo "Build succesful at $lastCommit"
-	node -v
+	./out/Release/node -v
 else
 	echo "Build failed. Check ../node-master-build.log"
+	tail -n 50 ../node-master-build.log
+	exit 1
 fi
-	
+popd
 
+ln -s $startDir/node/out/Release/node ./node
+ln -s $startDir/node/benchmark benchmark
 if [ -n "$FILTER" ]; then
 	FILTER="--filter ${FILTER}"
 fi
 if [ -n "$RUNS" ]; then
 	RUNS="--runs ${RUNS}"
 fi
-postbuild=`date +%s`
+postBuild=`date +%s`
 # run benchmark
 fileName=output${CATEGORY}-`date +%d%m%y-%H%M%S`.csv
 echo "Output will be saved to $fileName"
@@ -66,7 +72,7 @@ pwd
 cat $fileName | awk -f summariseCSV.awk
 mv $fileName $startDir
 scriptend=`date +%s`
-let buildTime = $postBuild - $start
-let postbuildTime = $scriptend - $postBuild
+let buildTime=postBuild-startTime
+let postbuildTime=scriptend-postBuild
 echo "Build took $buildTime seconds"
-echo "Benchmark took $ipostbuildiTime seconds"
+echo "Benchmark took $postbuildTime seconds"
